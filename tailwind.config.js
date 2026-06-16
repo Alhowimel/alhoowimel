3d28262a9c2165c5d77cf138168b5315e62382d6
Alhowimel <Xlly8794@gmail.com>
2026-06-16 11:31:22 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#C9A84C',
+        navy: '#1A2744',
+      },
+      fontFamily: {
+        sans: ['Tajawal', 'Cairo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
